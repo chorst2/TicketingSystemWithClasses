df3eca2f1304c9b408c5d0e19aa3f381534a1c70
Casidy Horst <CHorst2@my.wctc.edu>
2021-02-23 11:17:03 -0600

    creating tickets class

--- Tickets.cs
+++ Tickets.cs
@@ -5,6 +5,17 @@ namespace TicketingSystemWithClasses
 {
     class Tickets
     {
-        
+         public string ticketID;                        
+         public string ticketSummary;
+                     
+        public string ticketStatus;
+        public string ticketPriority;
+        public string ticketSubmitter;
+        public string ticketAssigned;
+        public string ticketWatching;
+
+        public string Display(){
+            return $"Ticket Id: {ticketID}\nTIcket Summary: {ticketSummary}\nTicket Status: {ticketStatus}\nTicket Priority: {ticketPriority}\nTicket Submitter: {ticketSubmitter}\nTicket Assigned: {ticketAssigned}\nTicket Watching: {ticketWatching}";
+        }
     }
 }
