18764888079a122563ca48799fa15a691ca05e36
Casidy Horst <CHorst2@my.wctc.edu>
2021-02-23 22:35:59 -0600

    creating AddTicket() method

--- TicketsFIle.cs
+++ TicketsFIle.cs
@@ -2,6 +2,7 @@ using System;
 using System.IO;
 using NLog.Web;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TicketingSystemWithClasses
 {
@@ -18,7 +19,37 @@ namespace TicketingSystemWithClasses
             ticketsFile = file;
             Ticket = new List<Tickets>();
 
+            try{
+                StreamReader sr = new StreamReader(ticketsFile);
+
+                sr.ReadLine();
+                while(!sr.EndOfStream)
+                {
+                    Tickets ticket = new Tickets();
+                    string line = sr.ReadLine();
+
+                    string[] ticketDetails = line.Split(',');
+                    ticket.ticketID = ticketDetails[1];
+                    ticket.ticketSummary = ticketDetails[2];
+                    ticket.ticketStatus = ticketDetails[3];
+                    ticket.ticketPriority = ticketDetails[4];
+                    ticket.ticketSubmitter = ticketDetails[5];
+                    ticket.ticketAssigned = ticketDetails[6];
+                    ticket.ticketWatching = ticketDetails[7].Replace('|',', ');
+                    Ticket.Add(ticket);
+                }
+                sr.Close();
+                logger.Info("Tickets in file {Count}", Ticket.Count);
+            }catch(Exception ex){
+                logger.Error(ex.Message);
+            }
+        }
+
+        public void AddTicket(Tickets ticket){
+            try{
+                ticket.ticketID = Ticket.Max(t => t.ticketID) + 1;
             
+            }
         }
     }
 }
