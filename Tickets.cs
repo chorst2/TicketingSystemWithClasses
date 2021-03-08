732c7a12a4183f8431d10df6518f6a5e3020ab2b
Casidy Horst <CHorst2@my.wctc.edu>
2021-03-08 12:55:21 -0600

    creating bug and enhancement class

--- Tickets.cs
+++ Tickets.cs
@@ -3,19 +3,39 @@ using System.IO;
 
 namespace TicketingSystemWithClasses
 {
-    class Tickets
+    public abstract class Tickets
     {
-         public string ticketID;                  
-         public string ticketSummary;
+         public string ticketID {get; set;}                  
+         public string ticketSummary {get; set;}
                      
-        public string ticketStatus;
-        public string ticketPriority;
-        public string ticketSubmitter;
-        public string ticketAssigned;
-        public string ticketWatching;
+        public string ticketStatus {get;set;}
+        public string ticketPriority {get; set;}
+        public string ticketSubmitter {get; set;}
+        public string ticketAssigned {get; set;}
+        public List<string> ticketWatching {get; set;}
 
-        public string Display(){
-            return $"Ticket Id: {ticketID}\nTIcket Summary: {ticketSummary}\nTicket Status: {ticketStatus}\nTicket Priority: {ticketPriority}\nTicket Submitter: {ticketSubmitter}\nTicket Assigned: {ticketAssigned}\nTicket Watching: {ticketWatching}";
+        public Tickets()
+        {
+            ticketWatching = new List<string>();
         }
+
+        public virtual string Display(){
+            return $"Ticket Id: {ticketID}\nTIcket Summary: {ticketSummary}\nTicket Status: {ticketStatus}\nTicket Priority: {ticketPriority}\nTicket Submitter: {ticketSubmitter}\nTicket Assigned: {ticketAssigned}\nTicket Watching: {string.Join(", ", ticketWatching)}\n";
+        }
+    }
+
+    public class Bug : Tickets{
+        public string ticketSeverity {get; set;}
+
+        public override string Display(){
+            return $"Ticket Id: {ticketID}\nTIcket Summary: {ticketSummary}\nTicket Status: {ticketStatus}\nTicket Priority: {ticketPriority}\nTicket Submitter: {ticketSubmitter}\nTicket Assigned: {ticketAssigned}\nTicket Watching: {string.Join(", ", ticketWatching)}\nTicket Severity: {ticketSeverity}\n";
+        }
+    }
+
+    public class Enhancement : Tickets{
+        public string ticketSoftware {get; set;}
+        public decimal ticketCost {get; set;}
+        public string ticketReason {get; set;}
+        public string ticketEstimate {get; set;}
     }
 }
